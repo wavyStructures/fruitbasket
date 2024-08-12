5601ce18643382106055d8db31f621df99a0bd44
wavyStructures <anja_schwab@gmx.de>
2024-08-12 07:02:07 +0200

    moving singlefruit html from fruitlist.c.html

--- src/app/fruitlist/fruitlist.component.ts
+++ src/app/fruitlist/fruitlist.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SingleFruitComponent } from './singlefruit/singlefruit.component';
 
 @Component({
 selector: 'app-fruitlist',
 standalone: true,
-imports: [CommonModule],
+imports: [CommonModule, SingleFruitComponent],
 templateUrl: './fruitlist.component.html',
 styleUrl: './fruitlist.component.scss'
 })
 export class FruitlistComponent{
+
+  fontColorGood = 'green';
+  fontColorBad = 'red';
 
     fruitlist = [
         {
---
+++ src/app/fruitlist/singlefruit/singlefruit.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+
+
+
+@Component({
+    selector: 'app-singlefruit',
+    standalone: true,
+    imports: [],
+    templateUrl: './singlefruit.component.html',
+    styleUrl: './singlefruit.component.scss'
+})
+export class SingleFruitComponent{
+ @Input()fruit = { name: "Apfel",
+    img:"apple.png",
+    description: "Äpfel sind aufgrund ihres hohen Wassergehalts kalorienarm und enthalten nur Spuren von Fett und Eiweiß, dafür aber rund zwei Prozent Ballaststoffe und etwa elf Prozent Kohlenhydrate. Äpfel enthalten auch viele Vitamine und Mineralstoffe und sind daher eine wichtige Quelle für uns - zum Beispiel für Vitamin C.",
+    genus: "Kernobstgewächsen innerhalb der Familie der Rosengewächse",
+    stars: 2.3,
+    reviews:[{name: "Kevin W.", text: "ist lecker"},{name: "Arne P.", text: "nicht so meins"}],
+};
+}
+
